10b085f55d476be1bbe4a591db3cda5151e255aa
kevin beier <kevinb@codingtemple.com>
2023-05-09 15:38:47 -0400

    remove migration folders

--- grader.py
+++ grader.py
@@ -2,7 +2,17 @@ import os
 import shutil
 
 students ={
-    "John-react":"https://github.com/CrtlAltElite/CRAVE",
+    # # "Jurgita":"",
+    # "sheridan":"https://github.com/BigBird29575/shopping_cart",
+    # "jeremy":"https://github.com/jermateo/shopping_cart",
+    # # "desiree":"",
+    # "mani":"https://github.com/MemoriesWholesale/PirateShop.git",
+    # "mason":"https://github.com/masonsinner/shopping_cart_python",
+    # "leisa":"https://github.com/leisatrisler/wk2-python-basics-project",
+    # "Val":"https://github.com/ValenciaW9/PythonShoppingCart.git",
+    # # "keanu":"",
+    # # "austin":"",
+    # "Cyn":"https://github.com/Melikrome1/shoppingcart/blob/main/Untitled.ipynb" 
 }
 
 
@@ -11,7 +21,11 @@ for name, url in students.items():
     os.system(f"git clone {url} {name}")
     if os.path.isfile("./.env"):
         shutil.copyfile("./.env", f"./{name}/.env")
+        print(name)
         os.chdir(f"{name}")
+        if os.path.isdir("./migrations"):
+            print("here")
+            shutil.rmtree('migrations')
         os.system(f". ../flask-load.sh")
         os.chdir(f"..")
     if os.path.isfile(f"./{name}/package.json"):
